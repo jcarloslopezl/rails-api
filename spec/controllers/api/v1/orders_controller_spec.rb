ed52f3cd931a84968cd5a968a66cd2a522bfe6b9
Juan Carlos López <jcarloslopezl@hotmail.com>
2017-02-05 21:05:34 -0600

    Adds a custom order product serializer to remove the user association

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -22,13 +22,25 @@ describe Api::V1::OrdersController do
     before(:each) do
       current_user = FactoryGirl.create :user
       api_authorization_header current_user.auth_token
-      @order = FactoryGirl.create :order, user: current_user
+
+      @product = FactoryGirl.create :product
+      @order = FactoryGirl.create :order, user: current_user, product_ids: [@product.id]
       get :show, params: { user_id: current_user.id, id: @order.id }
     end
 
     it "returns the user order record matching the id" do
       order_response = json_response[:order]
       expect(order_response[:id]).to eql @order.id
+    end
+
+    it "includes the total for the order" do
+      order_response = json_response[:order]
+      expect(order_response[:total]).to eql @order.total.to_s
+    end
+
+    it "includes the products on the order" do
+      order_response = json_response[:order]
+      expect(order_response[:products]).to have(1).item
     end
 
     it { should respond_with 200 }
@@ -42,7 +54,7 @@ describe Api::V1::OrdersController do
       product_1 = FactoryGirl.create :product
       product_2 = FactoryGirl.create :product
       order_params = { product_ids: [product_1.id, product_2.id] }
-      post :create, user_id: current_user.id, order: order_params
+      post :create, params: { user_id: current_user.id, order: order_params }
     end
 
     it "returns the just user order record" do
@@ -52,5 +64,5 @@ describe Api::V1::OrdersController do
 
     it { should respond_with 201 }
   end
-  
+
 end
